e0b23a5d60f8c55783987a323ac16c4b4ad003c9
Armin Schaefer <arminnessie@gmail.com>
2025-09-17 13:49:32 +0200

    bevy 0.17 fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use bevy::math::Vec3;
-use bevy::render::mesh::{Indices, Mesh, PrimitiveTopology};
-use bevy::render::render_asset::RenderAssetUsages;
+use bevy::mesh::{Indices, Mesh, PrimitiveTopology};
+use bevy::asset::RenderAssetUsages;
 
 /// A 2D or 3D mesh (`bevy::render::mesh::Mesh`) generated from a single-variable function
 /// `f(f32) -> f32`.
